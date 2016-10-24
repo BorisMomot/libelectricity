98403f0cb600076cdd4bd89e3f55dc67d8c333e0
boris <boris@server.setri.ru>
2016-10-25 00:30:47 +0300

    Наполнил ДГ формулами

--- DG.cpp
+++ DG.cpp
@@ -1,8 +1,63 @@
 
-#include "Source.h"
+#include "DG.h"
+#include <math.h>
 
 DG::DG(unsigned int Pnominal, unsigned int Qnominal, unsigned int Unominal) : Source(Pnominal, Qnominal, Unominal)
 {
+    computeJ();
+}
+
+void DG::calculateSourceF(unsigned int dTime) {
+    Source::calculateSourceF(dTime);
+
+
+    computeFuelFlow(dTime);
+    computeMdv(dTime);
+    computeMnagr(dTime);
+    computeRPM(dTime);
+    f = RPM / 60;
+}
+
+void DG::computeJ() {
+    J = (Pnom/314) * (Pnom/314)/20;
+}
+
+void DG::computeFuelFlow(unsigned int dTime) {
+    FuelFlow_ps = FuelFlow;
+    //защищаемся от переполнения
+    if (fabs(integrMismatch) < 1e300){
+        if ((fabs(f) < 1e200) && fabs(targetF) < 1e200) {
+            integrMismatch += (targetF - f);
+        }
+    }
+    else {
+        if (!signbit(integrMismatch)) {
+            integrMismatch = 1e300;
+        }
+        else {
+            integrMismatch = -1e300;
+        }
+    }
+    if (Ti != 0) {
+        FuelFlow = Kp * (targetF - f) + 1/Ti * Kp * dTime * integrMismatch;
+    }
+    else {
+        FuelFlow = -1;
+    }
+}
+
+void DG::computeMdv(unsigned int dTime) {
+    Mdv_ps = Mdv;
+    Mdv = ( FuelFlow*dTime + Mdv_ps*0.5 )/ (0.5 + dTime);
+}
+
+void DG::computeMnagr(unsigned int dTime) {
+    Mnagr = P / (f*2);
+}
+
+void DG::computeRPM(unsigned int dTime) {
+    RPM_ps = RPM;
+    RPM = RPM_ps + (Mdv - Mnagr)/J * dTime *60/2/M_PI;
 }
 
 
--- DG.h
+++ DG.h
@@ -11,6 +11,37 @@ class DG: public Source {
 public:
 	DG(unsigned int Pnominal, unsigned int Qnominal, unsigned int Unominal=400);
 	virtual ~DG(){}
+
+	virtual void calculateSourceF(unsigned int dTime) override;
+
+	double getJ() const { return J; }
+	double getFuelFlow() const { return FuelFlow; }
+	double getMdv() const { return Mdv; }
+	double getMnagr() const { return Mnagr;	}
+	double getRPM() const {	return RPM; }
+
+	void setKp(double Kp) {	DG::Kp = Kp; }
+	void setTi(double Ti) { DG::Ti = Ti; }
+protected:
+	inline void computeJ(); //
+	void computeFuelFlow(unsigned int dTime); //расчет уровня топлива подаваемого в дизель (ПИД регулятор)
+	void computeMdv(unsigned int dTime); //расчет момента дизеля
+	void computeMnagr(unsigned int dTime); //расчет текущего момента нагрузки
+	void computeRPM(unsigned int dTime); //расчет текущей скорости вращения ДГ
+
+	double J = {0}; //момент инерции ДГ
+	double FuelFlow = {0}; //подача топлива
+	double FuelFlow_ps = {0}; //подача топлива на предыдущем шаге
+	double Mdv = {0}; //момент дизеля
+	double Mdv_ps = {0}; //момент дизеля на предыдущем шаге
+	double Mnagr = {0}; //момент нагрузки от генератора
+	double RPM = {0}; //скорость вращения ДГ
+	double RPM_ps = {0}; //скорость вращения ДГ на предыдущем шаге
+
+	//Настройки для ПИ-регулятора
+	double Kp = {0}; //коэффициент усиления
+	double Ti = {1}; //коэффициент интегрирования
+	double integrMismatch = {0}; //хранит значение интегратора
 };
 
 
